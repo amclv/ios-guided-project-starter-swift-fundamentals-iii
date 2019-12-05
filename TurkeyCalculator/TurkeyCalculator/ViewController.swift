bae27d20d45a58bb33dd81ffd67aa694875d542a
Aaron Cleveland <toscleveland@gmail.com>
2019-12-05 14:07:27 -0600

    Finished Starter Project

--- TurkeyCalculator/TurkeyCalculator/ViewController.swift
+++ TurkeyCalculator/TurkeyCalculator/ViewController.swift
@@ -20,11 +20,52 @@ class ViewController: UIViewController {
     
     @IBAction func calculateButtonPressed(_ sender: Any) {
         
+        guard let turkeyWeightString = turkeyWeightTextField.text else { return }
+        
+        // see if the text in the text field is a valid Double
+        
+        guard let turkeyWeight = Double(turkeyWeightString) else {
+            print("Invalid number. Unable to init a double from the string.")
+            return
+        }
+        
+        // based on the metric button's .isSelected prop, call one of the cook time functions
+        var durationInMinutes: Double
+        
+        if metricButton.isSelected {
+            durationInMinutes = cookTimeInKilograms(weight: turkeyWeight)
+        } else {
+            durationInMinutes = cookTimeInPounds(weight: turkeyWeight)
+        }
+        
+        // Show cook time in minutes
+        
+        cookTimeTextField.text = "Cook for \(durationInMinutes) minutes"
     }
     
     @IBAction func metricButtonPressed(_ sender: Any) {
+        metricButton.isSelected.toggle()
         
     }
     
     // Helper functions
+    
+    func cookTimeInPounds(weight: Double) -> Double {
+        let durationInMinutes = weight * 15.0
+        return durationInMinutes
+        // could do just
+        // return weight * 15.0
+    }
+    
+    func cookTimeInKilograms(weight: Double) -> Double {
+        var durationInMinutes: Double
+        
+        if weight >= 4 {
+            durationInMinutes = 20.0 * weight + 90
+        } else {
+            durationInMinutes = 20.0 * weight + 70
+        }
+        
+        return durationInMinutes
+    }
 }
